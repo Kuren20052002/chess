df9a03b82ea8f9b1495f2dec950ab7c8092d7075
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-08-24 17:52:06 +0700

    Add valid_move for horsee

--- lib/knight.rb
+++ lib/knight.rb
@@ -1,5 +1,24 @@
 require_relative "piece"
 class Knight < Piece
+  def initialize(row, column, side)
+    super
+    @moveset = [[-1, -2], [-2, -1], [1, -2], [2, -1], [2, 1], [1, 2], [-1, 2], [-2, 1]]
+  end
+
+  def legal_moves
+    legal_moves = @moveset.map do |row, column|
+      [(@row + row), (@column + column)]
+    end
+  end
+
+  def valid_move?(move, board)
+    moves = legal_moves
+    moves.each do |row, column|
+      return true if move == [row, column] && (board[row][column] == " " || board[row][column].side != @side)
+    end
+    false
+  end
+
   def to_s
     @side == "white" ? "\u2658" : "\u265E"
   end
---
+++ spec/knight_spec.rb
@@ -0,0 +1,54 @@
+require_relative "../lib/board"
+
+describe Knight do
+  describe "#legal_moves" do
+    context "when the knight is on its initial position" do
+      subject(:white_knight) { described_class.new(0, 1, "white") }
+      subject(:black_knight) { described_class.new(7, 6, "black") }
+      it "can jump around" do
+        expect(white_knight.legal_moves).to include([2, 0], [2, 2], [1, -1], [1, 3])
+        expect(black_knight.legal_moves).to include([5, 5], [5, 7], [6, 4], [6, 8])
+      end
+    end
+  end
+
+  describe "#valid_move?" do
+    let(:board) { Board.new }
+
+    before do
+      board.place_piece
+    end
+
+    context "when the input is random number" do
+      subject(:white_knight) { described_class.new(0, 1, "white") }
+      it "return false" do
+        expect(white_knight.valid_move?([7, 2], board)).to be false
+        expect(white_knight.valid_move?([4, 0], board)).to be false
+        expect(white_knight.valid_move?([3, 8], board)).to be false
+        expect(white_knight.valid_move?([5, 9], board)).to be false
+      end
+    end
+
+    context "when knight is on the initial square" do
+      subject(:white_knight) { described_class.new(0, 1, "white") }
+      it "can jump over pawn" do
+        expect(white_knight.valid_move?([2, 2], board)).to be true
+      end
+    end
+
+    context "when there are obstruction" do
+      subject(:white_knight) { described_class.new(4, 0, "white") }
+      before do
+        board.move_piece([1, 2], [5, 2])
+      end
+
+      it "can eat black pawn" do
+        expect(white_knight.valid_move?([6, 1], board)).to be true
+      end
+
+      it "can't eat white pawn" do
+        expect(white_knight.valid_move?([5, 2], board)).to be false
+      end
+    end
+  end
+end
