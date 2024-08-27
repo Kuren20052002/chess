56bbced5d8d23d71483f7040b44910db02aab2bd
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-08-27 08:41:21 +0700

    Fix edge case in valid_move?

--- lib/player.rb
+++ lib/player.rb
@@ -6,27 +6,24 @@ class Player
   end
 
   def make_move
-    start = gets.chomp.downcase
-    until valid_move?(start)
+    start, destination = gets.chomp.downcase.split
+    until valid_move?(start) && valid_move?(destination)
       puts "Please input a valid square on the board"
-      start = gets.chomp.downcase
+      start, destination = gets.chomp.downcase.split
     end
 
-    endo = gets.chomp.downcase
-    until valid_move?(endo)
-      puts "Please input a valid square on the board"
-      endo = gets.chomp.downcase
-    end
-    [translate_pos(start), translate_pos(endo)]
+    [translate_pos(start), translate_pos(destination)]
   end
 
   private
 
   def valid_move?(move)
+    return false unless move
+
     move.match?(/^[a-h][1-8]$/)
   end
 
   def translate_pos(pos)
-    [pos[0].ord - 97, pos[1].to_i - 1]
+    [pos[1].to_i - 1, pos[0].ord - 97]
   end
 end
